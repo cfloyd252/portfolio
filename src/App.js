e9af5ed8fcc61aeda7de48f660f2b22a6b382f64
cfloyd252 <cfloyd252@gmail.com>
2020-06-05 16:35:58 -0700

    working slider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable default-case */
+import React, { Component } from 'react'
+import Slider from '@material-ui/core/Slider';
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const marks = [
+  {
+    value: 0,
+    label: <Link to='/'>Welcome</Link>,
+  },
+  {
+    value: 1,
+    label: <Link to='/bio'>Bio</Link>,
+  },
+  {
+    value: 2,
+    label: <Link to='/projects'>Projects</Link>,
+  },
+];
+
+export class App extends Component {
+  state = {
+    sectionView: 0
+  }
+
+  updateSectionView = (value) => {
+    switch(value) {
+      case 0:
+        this.props.history.push(`/`)
+        break;
+      case 1:
+        this.props.history.push(`/bio`)
+        break;
+      case 2:
+        this.props.history.push(`/projects`)
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Slider 
+          defaultValue={0}
+          onChange={(e, value) => this.updateSectionView(value)}
+          aria-labelledby="discrete-slider-custom"
+          step={1}
+          marks={marks}
+          max={2}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App);
