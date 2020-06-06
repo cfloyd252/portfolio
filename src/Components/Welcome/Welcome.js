728cef6f6cc6621c199da5c26276ea59fdce423a
cfloyd252 <cfloyd252@gmail.com>
2020-06-06 02:30:24 -0700

    completed welcome page

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import './Welcome.css'
 
 export class Welcome extends Component {
     render() {
         return (
             <main id='welcome'>
-                <h1>Welcome</h1>
                 <h2 id="opening_statement">My name is Chris and I went to math camp.</h2>
+                <img src={require('../../images/fallout_suit.jpeg')}/>
             </main>
         )
     }
