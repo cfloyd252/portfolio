3d119ca1805d965fa3b33989d58e056ef1144d1a
cfloyd252 <cfloyd252@gmail.com>
2020-06-06 02:58:29 -0700

    fixed welcome

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -4,7 +4,21 @@ export class Bio extends Component {
     render() {
         return (
             <main id='bio'>
-                bio
+                <p>
+                  Going from a small town boy with big aspirations to being able to call 
+                  myself a father and web developer has been quite a journey. It all 
+                  started with going to math camp (Well, it was more of a math and science 
+                  honors program that high school students attended during their summer 
+                  break). That’s what sparked my interest in coding. My first 
+                  experience with this new found passion by becoming the head programmer 
+                  for my high school robotics team for the remaining 3 years of high school.
+                  From there, I continued to take Computer Science college courses while 
+                  raising and providing for two wonderful boys. Any free time I had was 
+                  spent either going on runs or playing video games on my custom built 
+                  computer. After spending years in customer service related jobs and now 
+                  that my boys are becoming more independent, I finally found the time and 
+                  opportunity to rekindle my passion for coding.
+                </p>
             </main>
         )
     }
