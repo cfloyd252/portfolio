3127ad09e1e1e336c3b5ae9b5dd9557ad59f7c7b
cfloyd252 <cfloyd252@gmail.com>
2020-09-10 02:22:44 -0700

    reformed using hooks

--- src/Components/Games/TwentyFortyEight/TwentyFortyEight.js
+++ src/Components/Games/TwentyFortyEight/TwentyFortyEight.js
@@ -1,32 +1,59 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import cloneDeep from 'lodash.clonedeep';
 import './TwentyFortyEight.css'
 
-export class TwentyFortyEight extends Component {
-  state = {
-    data: [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0]
-    ]
+function TwentyFortyEight() {
+  const [gridData, setGridData] = useState([
+    [ 0, 0, 0, 0],
+    [ 0, 0, 0, 0],
+    [ 0, 0, 0, 0],
+    [ 0, 0, 0, 0]
+  ])
+
+  // Initialize
+  const initGrid = () => {
+    let newGrid = cloneDeep(gridData)
+    let newClone = cloneDeep(gridData)
+
+    addNewNumber(newGrid)
+    addNewNumber(newGrid)
   }
 
-  render() {
-    const displayIndexValues = this.state.data.map( row => {
-      return row.map(indexValue => {
-        return <div>{indexValue}</div>
-      })
+  //Add new number
+  const addNewNumber = (grid) => {
+    let added = false;
+    let gridFull = false;
+
+    while(!added) {
+      if (gridFull) {
+        break;
+      }
+
+      let rand1 = Math.floor(Math.random() * 4)
+      let rand2 = Math.floor(Math.random() * 4)
+
+      if (grid[rand1][rand2] === 0) {
+        grid[rand1][rand2] = Math.random() > 0.5 ? 2 :4;
+        added = true;
+      }
+    }
+  }
+
+  const displayGrid = gridData.map( row => {
+    return row.map(indexValue => {
+      return <div>{indexValue}</div>
     })
+  })
 
-    return (
-      <div id='TwentyFortyEight'>
-        <h1>2048</h1>
-        <div id='grid'>
-          {displayIndexValues}
-        </div>
+  return (
+    <div id='TwentyFortyEight'>
+      <h1>2048</h1>
+      <div id='grid'>
+        {displayGrid}
       </div>
-    )
-  }
+      <button onClick={() => initGrid()}></button>
+    </div>
+  )
 }
 
 export default TwentyFortyEight
